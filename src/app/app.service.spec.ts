9f774d81a01789d735d12b14608eb4a86cb53109
Voon Wong <voon.wong@smartbear.com>
2025-07-21 09:40:41 +1000

    chore: update tests

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -4,7 +4,7 @@ describe('AppService', () => {
   let service: AppService;
 
   beforeEach(() => {
-    window.location.hash = '';
+    window.sessionStorage.setItem('hash', '');
     service = new AppService();
   });
 
@@ -15,49 +15,49 @@ describe('AppService', () => {
   it('should set custom theme', () => {
     service.setTheme('Cosmo');
     expect(service.getTheme()).toBe('Cosmo');
-    expect(window.location.hash).toBe('#theme=Cosmo');
+    expect(window.sessionStorage.getItem('hash')).toBe('theme=Cosmo');
   });
 
   it('should set default theme', () => {
     service.setTheme('Default');
     expect(service.getTheme()).toBe('Default');
-    expect(window.location.hash).toBe('');
+    expect(window.sessionStorage.getItem('hash')).toBe('');
   });
 
   it('should set 2 column layout', () => {
     service.setLayout('2');
     expect(service.getLayout()).toBe('2');
-    expect(window.location.hash).toBe('');
+    expect(window.sessionStorage.getItem('hash')).toBe('');
   });
 
   it('should set 3 column layout', () => {
     service.setLayout('3');
     expect(service.getLayout()).toBe('3');
-    expect(window.location.hash).toBe('#layout=3');
+    expect(window.sessionStorage.getItem('hash')).toBe('layout=3');
   });
 
   it('should set HTTP OPTIONS', () => {
     service.setHttpOptions(true);
     expect(service.getHttpOptions()).toBe(true);
-    expect(window.location.hash).toBe('#httpOptions=true');
+    expect(window.sessionStorage.getItem('hash')).toBe('httpOptions=true');
   });
 
   it('should unset HTTP OPTIONS', () => {
     service.setHttpOptions(false);
     expect(service.getHttpOptions()).toBe(false);
-    expect(window.location.hash).toBe('');
+    expect(window.sessionStorage.getItem('hash')).toBe('');
   });
 
   it('should set all HTTP methods for links', () => {
     service.setAllHttpMethodsForLinks(true);
     expect(service.getAllHttpMethodsForLinks()).toBe(true);
-    expect(window.location.hash).toBe('#allHttpMethodsForLinks=true');
+    expect(window.sessionStorage.getItem('hash')).toBe('allHttpMethodsForLinks=true');
   });
 
   it('should unset all HTTP methods for links', () => {
     service.setAllHttpMethodsForLinks(false);
     expect(service.getAllHttpMethodsForLinks()).toBe(false);
-    expect(window.location.hash).toBe('');
+    expect(window.sessionStorage.getItem('hash')).toBe('');
   });
 
   it('should not set invalid layout', () => {
@@ -66,7 +66,7 @@ describe('AppService', () => {
     service.setLayout('4');
 
     expect(service.getLayout()).toBe('2');
-    expect(window.location.hash).toBe('');
+    expect(window.sessionStorage.getItem('hash')).toBe('');
     expect(window.console.error).toHaveBeenCalled();
   });
 
@@ -82,11 +82,11 @@ describe('AppService', () => {
     expect(service.getCustomRequestHeaders()[0].value).toBe('application/json');
     expect(service.getCustomRequestHeaders()[1].key).toBe('authorization');
     expect(service.getCustomRequestHeaders()[1].value).toBe('bearer euztsfghfhgwztuzt');
-    expect(window.location.hash).toBe('#hkey0=accept&hval0=application/json&hkey1=authorization&hval1=bearer%20euztsfghfhgwztuzt');
+    expect(window.sessionStorage.getItem('hash')).toBe('hkey0=accept&hval0=application/json&hkey1=authorization&hval1=bearer euztsfghfhgwztuzt');
   });
 
-  it('should parse window location hash', () => {
-    window.location.hash = '#theme=Cosmo&layout=3&httpOptions=true&allHttpMethodsForLinks=true&hkey0=accept&hval0=text/plain&uri=https://chatty42.herokuapp.com/api/users';
+  it('should parse sessionStorage "hash" item', () => {
+    window.sessionStorage.setItem('hash', 'theme=Cosmo&layout=3&httpOptions=true&allHttpMethodsForLinks=true&hkey0=accept&hval0=text/plain&uri=https://chatty42.herokuapp.com/api/users');
     service = new AppService();
 
     expect(service.getCustomRequestHeaders()[0].key).toBe('accept');
@@ -98,8 +98,8 @@ describe('AppService', () => {
     expect(service.getUri()).toBe('https://chatty42.herokuapp.com/api/users');
   });
 
-  it('should parse window location hash with hval before hkey', () => {
-    window.location.hash = '#theme=Cosmo&layout=3&hval0=text/plain&hkey0=accept&uri=https://chatty42.herokuapp.com/api/users';
+  it('should parse sessionStorage "hash" item with hval before hkey', () => {
+    window.sessionStorage.setItem('hash', 'theme=Cosmo&layout=3&hval0=text/plain&hkey0=accept&uri=https://chatty42.herokuapp.com/api/users');
     service = new AppService();
 
     expect(service.getCustomRequestHeaders()[0].key).toBe('accept');
@@ -109,8 +109,8 @@ describe('AppService', () => {
     expect(service.getUri()).toBe('https://chatty42.herokuapp.com/api/users');
   });
 
-  it('should parse window location hash with deprecated hkey "url"', () => {
-    window.location.hash = '#theme=Cosmo&layout=3&hval0=text/plain&hkey0=accept&url=https://chatty42.herokuapp.com/api/users';
+  it('should parse sessionStorage "hash" item with deprecated hkey "url"', () => {
+    window.sessionStorage.setItem('hash', 'theme=Cosmo&layout=3&hval0=text/plain&hkey0=accept&url=https://chatty42.herokuapp.com/api/users');
     service = new AppService();
 
     expect(service.getCustomRequestHeaders()[0].key).toBe('accept');
@@ -120,8 +120,8 @@ describe('AppService', () => {
     expect(service.getUri()).toBe('https://chatty42.herokuapp.com/api/users');
   });
 
-  it('should parse window location hash with unknown hkeys', () => {
-    window.location.hash = '#theme=Cosmo&xxx=7&layout=3&hval0=text/plain&hkey0=accept&yyy=xxx&url=https://chatty42.herokuapp.com/api/users';
+  it('should parse sessionStorage "hash" item with unknown hkeys', () => {
+    window.sessionStorage.setItem('hash', 'theme=Cosmo&xxx=7&layout=3&hval0=text/plain&hkey0=accept&yyy=xxx&url=https://chatty42.herokuapp.com/api/users');
     service = new AppService();
 
     expect(service.getCustomRequestHeaders()[0].key).toBe('accept');
